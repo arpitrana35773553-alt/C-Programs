60d2320f70459c102ed3737083f4825e9e83ad75
Arpit Rana <arpitrana35773553@gmail.com>
2026-03-10 21:13:22 +0530

    Added Today's Practice Problems

--- array.cpp
+++ array.cpp
@@ -15,3 +15,55 @@ int main(){
     cout<<"Smallest int is:"<<smallest<<endl;
     return 0;
 }
+
+/////////////////PROBLEM2/////////////////
+//PRINT THE INDEX NO. WHICH STORES THE SMALLEST VALUE IN THE ARRAY
+#include <iostream>
+using namespace std;
+int main(){
+    int num[5] = {12,34,67,-8,-11};
+    int size = sizeof(num)/sizeof(int);
+    int smallest = INT_MAX;
+    int small = 0;
+    for(int i = 0;i<size;i++){
+        small = min(num[i],smallest);
+        smallest = small;
+    }
+    int index = 0;
+    for(int i=0;i<size;i++){
+        if(num[i]==smallest){
+            cout<<"Index value of smallest value:"<<index<<endl;
+        }else{
+            index++;
+        }
+    }
+    return 0;
+}
+
+
+///////////////PROBLEM3/////////////////
+////FIND THE LARGEST VALUE IN THE ARRAY AND PRINT IT'S INDEX VALUE
+#include <iostream>
+using namespace std;
+int main(){
+    int arr[5] = {1,23,4,-3,12};
+    int size = sizeof(arr)/sizeof(int);
+    int largest = INT_MIN;
+    for(int i = 0;i<size;i++){
+        if(arr[i]>largest){
+            largest = arr[i];
+        }
+    }
+    cout<<"The largest value in array is:"<<largest<<endl;
+    
+    int index = 0;
+    for(int i = 0;i<size;i++){
+        if(arr[i]==largest){
+            cout<<"The index of largest value is:"<<index<<endl;
+        }else{
+            index++;
+        }
+    }
+    return 0;
+}
+
