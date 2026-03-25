1e8fc2c47c905fff628129f6cb3d822688efc5cf
Arpit Rana <arpitrana35773553@gmail.com>
2026-03-25 21:49:15 +0530

    Added LeetCode Two Sum solution

---
+++ leetcode.cpp
@@ -0,0 +1,20 @@
+///////////////25-03-2026///////////
+////PROBLEM: TWO SUM
+////PLATFORM: LEETCODE
+////Approach: Brute Force, Time Complexity: O(n^2), Space Complexity: O(1)
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> vec;
+        for(int i = 0;i < nums.size();i++){
+            for(int j = 0;j < nums.size();j++){
+                if(nums[i] + nums[j] == target && i != j){
+                   vec.push_back(i);
+                   vec.push_back(j);
+                   return vec;
+                }
+            }
+        }
+    return {};
+    }
+};
