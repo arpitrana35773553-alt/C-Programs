562ac2b48ddb2034e6a306af38907c927c8c5946
Arpit Rana <arpitrana35773553@gmail.com>
2026-09-01 22:14:43 +0530

    Added new programs

--- .vscode/recursion.cpp
+++ .vscode/recursion.cpp
@@ -1,3 +1,4 @@
+//Printing numbers 1 to n
 #include <iostream>
 using namespace std;
 int count = 0;
@@ -13,7 +14,7 @@ int main(){
     rec(count);
     return 0;
 }
-
+//Printing name n times
 #include <iostream>
 using namespace std;
 int count = 0;
@@ -30,6 +31,7 @@ int main(){
     rec(name,count);
 }
 
+//Printing 1 to n
 #include <iostream>
 using namespace std;
 int start = 1;
@@ -47,6 +49,7 @@ int main(){
     nums(start,n);
 }
 
+//Printing n to 1
 #include <iostream>
 using namespace std;
 int start_val = 0;
@@ -64,3 +67,107 @@ int main(){
     nums(start_val,start);
     return 0;
 }
+
+//Printing 1 to n by different method
+#include <iostream>
+using namespace std;
+void func(int i){
+    if(i < 1){
+        return;
+    }
+    func(i - 1);
+    cout << i << endl;
+}
+int main(){
+    int n;
+    cin >> n;
+    func(n);
+}
+
+
+#include <iostream>
+using namespace std;
+int i = 1;
+void func(int i,int n){
+    if(i > n){
+        return;
+    }
+    func(i + 1,n);
+    cout << i << endl;
+}
+int main(){
+    int n;
+    cin >> n;
+    func(i,n);
+}
+
+
+//Printing sum of 1 to n by parameterized way
+#include <iostream>
+using namespace std;
+void func(int i, int sum){
+    if(i < 1){
+        cout << "Sum = " << sum;
+        return;
+    }
+    func(--i,sum + i);
+}
+int main(){
+    int n;
+    cin >> n;
+    int sum = 0;
+    func(n,sum);
+}
+
+//Printing sum of 1 to n
+#include <iostream>
+using namespace std;
+int func(int n){
+    if(n == 0){
+        return 0;
+    }else{
+        return (n + func(n - 1));
+    }   
+}
+int main(){
+    int n;
+    cin >> n;
+    cout << func(n);
+}
+
+//Printing factorial of n 
+#include <iostream>
+using namespace std;
+void facto(int n,int fact){
+    if(n == 1){
+        cout << fact;
+        return;
+    }
+    facto(--n,fact*n);
+}
+int main(){
+    int n;
+    cin >> n;
+    int fact = 1;
+    facto(n,fact);
+}
+
+//Printing factorial using other approach
+//TIME AND SPACE COMPLEXITY = O(n);
+#include <iostream>
+using namespace std;
+int facto(int n){
+    if(n == 1){
+        return 1;
+    }
+    return n * facto(n - 1);
+}
+int main(){
+    int n;
+    cin >> n;
+    cout << facto(n);
+}
+
+
+
+
